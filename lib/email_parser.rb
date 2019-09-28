c5e9cff9832b5fdaaccd37b2cac55ee14b572474
Andrew Williams <1williamsaj@gmail.com>
2019-09-28 22:41:08 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -9,12 +9,12 @@ class EmailAddressParser
 
   attr_accessor :email_addresses # First, we use attr_accessor to create our get and set methods.
 
-  def initialize(email_addresses) # Second, we initialize our string of emails
+  def initialize(email_addresses) # Second, we initialize our string of emails.
     @email_addresses = email_addresses
   end
 
-  def parse
-    email_addresses.delete(',').split.uniq
+  def parse # Third, in our parse method, we call our string of emails and delete the commas between the emails.
+    email_addresses.delete(',').split.uniq # Then we use split to take each email and place it in
   end
 
 end
